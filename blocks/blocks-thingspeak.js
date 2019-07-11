d4554d5fd00e1549661bc53b0d9c073424a26e77
Gr4vity4 <prawared.bw@gmail.com>
2019-07-11 20:28:32 +0700

    Block design update

--- blocks/blocks-thingspeak.js
+++ blocks/blocks-thingspeak.js
@@ -2,104 +2,89 @@ Blockly.Blocks["thing_speak_core"] = {
   init: function() {
     this.appendDummyInput()
       .appendField("THING SPEAK");
-    this.appendDummyInput()
+    this.appendValueInput("API KEY")
+      .setCheck("String")
+      .setAlign(Blockly.ALIGN_RIGHT)
       .appendField(new Blockly.FieldImage(
         "https://image.flaticon.com/icons/svg/179/179543.svg",
         20,
         20,
         "*"))
-      .appendField("API KEY")
-      .appendField(new Blockly.FieldTextInput("                              "),
-        "API_KEY");
-    this.appendDummyInput("FIELD1")
+      .appendField("API KEY");
+    this.appendValueInput("FIELD1")
       .setCheck("String")
+      .setAlign(Blockly.ALIGN_RIGHT)
       .appendField(new Blockly.FieldImage(
         "https://image.flaticon.com/icons/svg/148/148953.svg",
         20,
         20,
         "*"))
-      .appendField("FIELD 1 : DATA IS")
-      .appendField(new Blockly.FieldVariable(null),
-        "INPUT_FIELD1");
-    this.setConnectionsHidden(true);
-    this.appendDummyInput("FIELD2")
+      .appendField("FIELD 1");
+    this.appendValueInput("FIELD2")
       .setCheck("String")
+      .setAlign(Blockly.ALIGN_RIGHT)
       .appendField(new Blockly.FieldImage(
         "https://image.flaticon.com/icons/svg/148/148953.svg",
         20,
         20,
         "*"))
-      .appendField("FIELD 2 : DATA IS")
-      .appendField(new Blockly.FieldVariable(null),
-        "INPUT_FIELD2");
-    this.setConnectionsHidden(true);
-    this.appendDummyInput("FIELD3")
+      .appendField("FIELD 2");
+    this.appendValueInput("FIELD3")
       .setCheck("String")
+      .setAlign(Blockly.ALIGN_RIGHT)
       .appendField(new Blockly.FieldImage(
         "https://image.flaticon.com/icons/svg/148/148953.svg",
         20,
         20,
         "*"))
-      .appendField("FIELD 3 : DATA IS")
-      .appendField(new Blockly.FieldVariable(null),
-        "INPUT_FIELD3");
-    this.setConnectionsHidden(true);
-    this.appendDummyInput("FIELD4")
+      .appendField("FIELD 3");
+    this.appendValueInput("FIELD4")
       .setCheck("String")
+      .setAlign(Blockly.ALIGN_RIGHT)
       .appendField(new Blockly.FieldImage(
         "https://image.flaticon.com/icons/svg/148/148953.svg",
         20,
         20,
         "*"))
-      .appendField("FIELD 4 : DATA IS")
-      .appendField(new Blockly.FieldVariable(null),
-        "INPUT_FIELD4");
-    this.setConnectionsHidden(true);
-    this.appendDummyInput("FIELD5")
+      .appendField("FIELD 4");
+    this.appendValueInput("FIELD5")
       .setCheck("String")
+      .setAlign(Blockly.ALIGN_RIGHT)
       .appendField(new Blockly.FieldImage(
         "https://image.flaticon.com/icons/svg/148/148953.svg",
         20,
         20,
         "*"))
-      .appendField("FIELD 5 : DATA IS")
-      .appendField(new Blockly.FieldVariable(null),
-        "INPUT_FIELD5");
-    this.setConnectionsHidden(true);
-    this.appendDummyInput("FIELD6")
+      .appendField("FIELD 5");
+    this.appendValueInput("FIELD6")
       .setCheck("String")
+      .setAlign(Blockly.ALIGN_RIGHT)
       .appendField(new Blockly.FieldImage(
         "https://image.flaticon.com/icons/svg/148/148953.svg",
         20,
         20,
         "*"))
-      .appendField("FIELD 6 : DATA IS")
-      .appendField(new Blockly.FieldVariable(null),
-        "INPUT_FIELD6");
-    this.setConnectionsHidden(true);
-    this.appendDummyInput("FIELD7")
+      .appendField("FIELD 6");
+    this.appendValueInput("FIELD7")
       .setCheck("String")
+      .setAlign(Blockly.ALIGN_RIGHT)
       .appendField(new Blockly.FieldImage(
         "https://image.flaticon.com/icons/svg/148/148953.svg",
         20,
         20,
         "*"))
-      .appendField("FIELD 7 : DATA IS")
-      .appendField(new Blockly.FieldVariable(null),
-        "INPUT_FIELD7");
-    this.setConnectionsHidden(true);
-    this.appendDummyInput("FIELD8")
+      .appendField("FIELD 7");
+    this.appendValueInput("FIELD8")
       .setCheck("String")
+      .setAlign(Blockly.ALIGN_RIGHT)
       .appendField(new Blockly.FieldImage(
         "https://image.flaticon.com/icons/svg/148/148953.svg",
         20,
         20,
         "*"))
-      .appendField("FIELD 8 : DATA IS")
-      .appendField(new Blockly.FieldVariable(null),
-        "INPUT_FIELD8");
-    this.setNextStatement(true);
+      .appendField("FIELD 8");
     this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
     this.setColour(230);
     this.setTooltip("");
     this.setHelpUrl("");
--- blocks/generators-thingspeak.js
+++ blocks/generators-thingspeak.js
@@ -1,42 +1,28 @@
 Blockly.JavaScript["thing_speak_core"] = function(block) {
-  var text_api_key = block.getFieldValue("API_KEY");
-  var variable_input_field1 = Blockly.JavaScript.variableDB_.getName(block.getFieldValue(
-    "INPUT_FIELD1"), Blockly.Variables.NAME_TYPE);
+  var value_api_key = Blockly.JavaScript.valueToCode(block,
+    "API KEY",
+    Blockly.JavaScript.ORDER_ATOMIC);
   var value_field1 = Blockly.JavaScript.valueToCode(block,
     "FIELD1",
     Blockly.JavaScript.ORDER_ATOMIC);
-  var variable_input_field2 = Blockly.JavaScript.variableDB_.getName(block.getFieldValue(
-    "INPUT_FIELD2"), Blockly.Variables.NAME_TYPE);
   var value_field2 = Blockly.JavaScript.valueToCode(block,
     "FIELD2",
     Blockly.JavaScript.ORDER_ATOMIC);
-  var variable_input_field3 = Blockly.JavaScript.variableDB_.getName(block.getFieldValue(
-    "INPUT_FIELD3"), Blockly.Variables.NAME_TYPE);
   var value_field3 = Blockly.JavaScript.valueToCode(block,
     "FIELD3",
     Blockly.JavaScript.ORDER_ATOMIC);
-  var variable_input_field4 = Blockly.JavaScript.variableDB_.getName(block.getFieldValue(
-    "INPUT_FIELD4"), Blockly.Variables.NAME_TYPE);
   var value_field4 = Blockly.JavaScript.valueToCode(block,
     "FIELD4",
     Blockly.JavaScript.ORDER_ATOMIC);
-  var variable_input_field5 = Blockly.JavaScript.variableDB_.getName(block.getFieldValue(
-    "INPUT_FIELD5"), Blockly.Variables.NAME_TYPE);
   var value_field5 = Blockly.JavaScript.valueToCode(block,
     "FIELD5",
     Blockly.JavaScript.ORDER_ATOMIC);
-  var variable_input_field6 = Blockly.JavaScript.variableDB_.getName(block.getFieldValue(
-    "INPUT_FIELD6"), Blockly.Variables.NAME_TYPE);
   var value_field6 = Blockly.JavaScript.valueToCode(block,
     "FIELD6",
     Blockly.JavaScript.ORDER_ATOMIC);
-  var variable_input_field7 = Blockly.JavaScript.variableDB_.getName(block.getFieldValue(
-    "INPUT_FIELD7"), Blockly.Variables.NAME_TYPE);
   var value_field7 = Blockly.JavaScript.valueToCode(block,
     "FIELD7",
     Blockly.JavaScript.ORDER_ATOMIC);
-  var variable_input_field8 = Blockly.JavaScript.variableDB_.getName(block.getFieldValue(
-    "INPUT_FIELD8"), Blockly.Variables.NAME_TYPE);
   var value_field8 = Blockly.JavaScript.valueToCode(block,
     "FIELD8",
     Blockly.JavaScript.ORDER_ATOMIC);
@@ -44,15 +30,15 @@ Blockly.JavaScript["thing_speak_core"] = function(block) {
   var code = `
   #EXTINC #include <HTTPClient.h> #END\n
   HTTPClient http;\n
-  http.begin("https://api.thingspeak.com/update?api_key=${text_api_key}\
-  &field1=string(${variable_input_field1})\
-  &field2=string(${variable_input_field2})\
-  &field3=string(${variable_input_field3})\
-  &field4=string(${variable_input_field4})\
-  &field5=string(${variable_input_field5})\
-  &field6=string(${variable_input_field6})\
-  &field7=string(${variable_input_field7})\
-  &field8=string(${variable_input_field8})");\n
+  http.begin("https://api.thingspeak.com/update?api_key=${value_api_key}\
+  &field1=string(${value_field1})\
+  &field3=string(${value_field2})\
+  &field4=string(${value_field3})\
+  &field5=string(${value_field4})\
+  &field6=string(${value_field5})\
+  &field2=string(${value_field6})\
+  &field7=string(${value_field7})\
+  &field8=string(${value_field8})");\n
   http.GET();\n
   http.end();`;
 
