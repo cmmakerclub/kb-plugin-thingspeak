a5939acf8846544484ab504817ff964991c10678
Gr4vity4 <prawared.bw@gmail.com>
2019-07-11 14:51:37 +0700

    Add thingspeak block

---
+++ blocks/blocks-thingspeak.js
@@ -0,0 +1,99 @@
+Blockly.Blocks["thing_speak_core"] = {
+  init: function() {
+    this.appendDummyInput()
+      .appendField("THING SPEAK");
+    this.appendDummyInput()
+      .appendField(new Blockly.FieldImage(
+        "https://image.flaticon.com/icons/svg/179/179543.svg",
+        20,
+        20,
+        "*"))
+      .appendField("API KEY")
+      .appendField(new Blockly.FieldTextInput("                              "),
+        "API_KEY");
+    this.appendValueInput("FIELD1")
+      .setCheck("String")
+      .appendField(new Blockly.FieldImage(
+        "https://image.flaticon.com/icons/svg/148/148953.svg",
+        20,
+        20,
+        "*"))
+      .appendField("FIELD 1 : DATA IS")
+      .appendField(new Blockly.FieldTextInput("              "),
+        "INPUT_FIELD1");
+    this.appendValueInput("FIELD2")
+      .setCheck("String")
+      .appendField(new Blockly.FieldImage(
+        "https://image.flaticon.com/icons/svg/148/148953.svg",
+        20,
+        20,
+        "*"))
+      .appendField("FIELD 2 : DATA IS")
+      .appendField(new Blockly.FieldTextInput("              "),
+        "INPUT_FIELD2");
+    this.appendValueInput("FIELD3")
+      .setCheck("String")
+      .appendField(new Blockly.FieldImage(
+        "https://image.flaticon.com/icons/svg/148/148953.svg",
+        20,
+        20,
+        "*"))
+      .appendField("FIELD 3 : DATA IS")
+      .appendField(new Blockly.FieldTextInput("              "),
+        "INPUT_FIELD3");
+    this.appendValueInput("FIELD4")
+      .setCheck("String")
+      .appendField(new Blockly.FieldImage(
+        "https://image.flaticon.com/icons/svg/148/148953.svg",
+        20,
+        20,
+        "*"))
+      .appendField("FIELD 4 : DATA IS")
+      .appendField(new Blockly.FieldTextInput("              "),
+        "INPUT_FIELD4");
+    this.appendValueInput("FIELD5")
+      .setCheck("String")
+      .appendField(new Blockly.FieldImage(
+        "https://image.flaticon.com/icons/svg/148/148953.svg",
+        20,
+        20,
+        "*"))
+      .appendField("FIELD 5 : DATA IS")
+      .appendField(new Blockly.FieldTextInput("              "),
+        "INPUT_FIELD5");
+    this.appendValueInput("FIELD6")
+      .setCheck("String")
+      .appendField(new Blockly.FieldImage(
+        "https://image.flaticon.com/icons/svg/148/148953.svg",
+        20,
+        20,
+        "*"))
+      .appendField("FIELD 6 : DATA IS")
+      .appendField(new Blockly.FieldTextInput("              "),
+        "INPUT_FIELD6");
+    this.appendValueInput("FIELD7")
+      .setCheck("String")
+      .appendField(new Blockly.FieldImage(
+        "https://image.flaticon.com/icons/svg/148/148953.svg",
+        20,
+        20,
+        "*"))
+      .appendField("FIELD 7 : DATA IS")
+      .appendField(new Blockly.FieldTextInput("              "),
+        "INPUT_FIELD7");
+    this.appendValueInput("FIELD8")
+      .setCheck("String")
+      .appendField(new Blockly.FieldImage(
+        "https://image.flaticon.com/icons/svg/148/148953.svg",
+        20,
+        20,
+        "*"))
+      .appendField("FIELD 8 : DATA IS")
+      .appendField(new Blockly.FieldTextInput("              "),
+        "INPUT_FIELD8");
+    this.setPreviousStatement(true, null);
+    this.setColour(230);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
---
+++ blocks/generators-thingspeak.js
@@ -0,0 +1,38 @@
+Blockly.JavaScript["thing_speak_core"] = function(block) {
+  var text_api_key = block.getFieldValue("API_KEY");
+  var text_input_field1 = block.getFieldValue("INPUT_FIELD1");
+  var value_field1 = Blockly.JavaScript.valueToCode(block,
+    "FIELD1",
+    Blockly.JavaScript.ORDER_ATOMIC);
+  var text_input_field2 = block.getFieldValue("INPUT_FIELD2");
+  var value_field2 = Blockly.JavaScript.valueToCode(block,
+    "FIELD2",
+    Blockly.JavaScript.ORDER_ATOMIC);
+  var text_input_field3 = block.getFieldValue("INPUT_FIELD3");
+  var value_field3 = Blockly.JavaScript.valueToCode(block,
+    "FIELD3",
+    Blockly.JavaScript.ORDER_ATOMIC);
+  var text_input_field4 = block.getFieldValue("INPUT_FIELD4");
+  var value_field4 = Blockly.JavaScript.valueToCode(block,
+    "FIELD4",
+    Blockly.JavaScript.ORDER_ATOMIC);
+  var text_input_field5 = block.getFieldValue("INPUT_FIELD5");
+  var value_field5 = Blockly.JavaScript.valueToCode(block,
+    "FIELD5",
+    Blockly.JavaScript.ORDER_ATOMIC);
+  var text_input_field6 = block.getFieldValue("INPUT_FIELD6");
+  var value_field6 = Blockly.JavaScript.valueToCode(block,
+    "FIELD6",
+    Blockly.JavaScript.ORDER_ATOMIC);
+  var text_input_field7 = block.getFieldValue("INPUT_FIELD7");
+  var value_field7 = Blockly.JavaScript.valueToCode(block,
+    "FIELD7",
+    Blockly.JavaScript.ORDER_ATOMIC);
+  var text_input_field8 = block.getFieldValue("INPUT_FIELD8");
+  var value_field8 = Blockly.JavaScript.valueToCode(block,
+    "FIELD8",
+    Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = "...;\n";
+  return code;
+};
