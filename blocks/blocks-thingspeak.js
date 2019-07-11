645f33dc2146f88c89c86eafee42aec8117c9afd
Gr4vity4 <prawared.bw@gmail.com>
2019-07-11 15:52:00 +0700

    Add send http request to thingspeak

--- blocks/blocks-thingspeak.js
+++ blocks/blocks-thingspeak.js
@@ -19,7 +19,7 @@ Blockly.Blocks["thing_speak_core"] = {
         20,
         "*"))
       .appendField("FIELD 1 : DATA IS")
-      .appendField(new Blockly.FieldTextInput("              "),
+      .appendField(new Blockly.FieldVariable(null),
         "INPUT_FIELD1");
     this.appendValueInput("FIELD2")
       .setCheck("String")
@@ -29,7 +29,7 @@ Blockly.Blocks["thing_speak_core"] = {
         20,
         "*"))
       .appendField("FIELD 2 : DATA IS")
-      .appendField(new Blockly.FieldTextInput("              "),
+      .appendField(new Blockly.FieldVariable(null),
         "INPUT_FIELD2");
     this.appendValueInput("FIELD3")
       .setCheck("String")
@@ -39,7 +39,7 @@ Blockly.Blocks["thing_speak_core"] = {
         20,
         "*"))
       .appendField("FIELD 3 : DATA IS")
-      .appendField(new Blockly.FieldTextInput("              "),
+      .appendField(new Blockly.FieldVariable(null),
         "INPUT_FIELD3");
     this.appendValueInput("FIELD4")
       .setCheck("String")
@@ -49,7 +49,7 @@ Blockly.Blocks["thing_speak_core"] = {
         20,
         "*"))
       .appendField("FIELD 4 : DATA IS")
-      .appendField(new Blockly.FieldTextInput("              "),
+      .appendField(new Blockly.FieldVariable(null),
         "INPUT_FIELD4");
     this.appendValueInput("FIELD5")
       .setCheck("String")
@@ -59,7 +59,7 @@ Blockly.Blocks["thing_speak_core"] = {
         20,
         "*"))
       .appendField("FIELD 5 : DATA IS")
-      .appendField(new Blockly.FieldTextInput("              "),
+      .appendField(new Blockly.FieldVariable(null),
         "INPUT_FIELD5");
     this.appendValueInput("FIELD6")
       .setCheck("String")
@@ -69,7 +69,7 @@ Blockly.Blocks["thing_speak_core"] = {
         20,
         "*"))
       .appendField("FIELD 6 : DATA IS")
-      .appendField(new Blockly.FieldTextInput("              "),
+      .appendField(new Blockly.FieldVariable(null),
         "INPUT_FIELD6");
     this.appendValueInput("FIELD7")
       .setCheck("String")
@@ -79,7 +79,7 @@ Blockly.Blocks["thing_speak_core"] = {
         20,
         "*"))
       .appendField("FIELD 7 : DATA IS")
-      .appendField(new Blockly.FieldTextInput("              "),
+      .appendField(new Blockly.FieldVariable(null),
         "INPUT_FIELD7");
     this.appendValueInput("FIELD8")
       .setCheck("String")
@@ -89,7 +89,7 @@ Blockly.Blocks["thing_speak_core"] = {
         20,
         "*"))
       .appendField("FIELD 8 : DATA IS")
-      .appendField(new Blockly.FieldTextInput("              "),
+      .appendField(new Blockly.FieldVariable(null),
         "INPUT_FIELD8");
     this.setPreviousStatement(true, null);
     this.setColour(230);
